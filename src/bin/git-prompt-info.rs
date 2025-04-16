28018b313fe3498870d49a1d3444c3d35a78e294
Michael McClimon <michael@mcclimon.org>
2025-04-15 21:54:16 -0400

    Restructure to be less imperative
    This is totally pointless, other than me wanting to write a bit of rust
    so I don't totally forget how.

--- src/bin/git-prompt-info.rs
+++ src/bin/git-prompt-info.rs
@@ -1,92 +1,107 @@
-use clap::App;
-use std::ffi::OsStr;
-use std::path::PathBuf;
-use std::process::{exit, Command};
+use std::io;
+use std::process::{exit, Command, Output};
+
+#[derive(Debug, Default)]
+struct PromptInfo {
+  sha: String,
+  head: String,
+  is_dirty: bool,
+  is_weird: bool,
+}
 
 fn main() {
-  App::new("git-prompt-info").get_matches();
-
-  let out = Command::new("git")
-    .args(&[
-      "--no-optional-locks",
-      "status",
-      "--branch",
-      "--porcelain=v2",
-    ])
-    .output();
-
-  if out.is_err() || !out.as_ref().unwrap().status.success() {
-    bail();
-  }
-
-  let out = out.unwrap();
-
-  let mut sha = "??";
-  let mut head = "??";
-  let mut is_dirty = false;
-
-  let stdout = String::from_utf8(out.stdout).expect("bad string");
-
-  for line in stdout.lines() {
-    if line.starts_with("# branch.oid") {
-      sha = &line.rsplit(" ").next().unwrap()[0..8];
-      continue;
-    }
-
-    if line.starts_with("# branch.head") {
-      head = line.rsplit(" ").next().unwrap();
-      continue;
-    }
-
-    if !line.starts_with("#") {
-      is_dirty = true;
-      break;
-    }
-  }
-
-  let is_weird = is_weird();
-
-  let prep = if head == "(detached)" { "at" } else { "on" };
-  let branch = if head == "(detached)" { sha } else { head };
-
-  println!(
-    "1 {} {} {} {}",
-    prep,
-    branch,
-    if is_dirty { 1 } else { 0 },
-    if is_weird { 1 } else { 0 },
-  );
+  let info = PromptInfo::new();
+  println!("{}", info);
 }
 
-fn is_weird() -> bool {
-  let out = Command::new("git")
-    .args(&["rev-parse", "--git-dir"])
-    .output();
+impl PromptInfo {
+  fn new() -> Self {
+    let out = Command::new("git")
+      .args(&[
+        "--no-optional-locks",
+        "status",
+        "--branch",
+        "--porcelain=v2",
+      ])
+      .output();
 
-  if out.is_err() || !out.as_ref().unwrap().status.success() {
-    bail();
+    maybe_bail(&out);
+
+    let mut info: PromptInfo = Default::default();
+
+    let stdout = String::from_utf8(out.unwrap().stdout).expect("bad string");
+
+    for line in stdout.lines() {
+      if line.starts_with("# branch.oid") {
+        info.sha = line.rsplit(" ").next().unwrap()[0..8].into();
+        continue;
+      }
+
+      if line.starts_with("# branch.head") {
+        info.head = line.rsplit(" ").next().unwrap().into();
+        continue;
+      }
+
+      if !line.starts_with("#") {
+        info.is_dirty = true;
+        break;
+      }
+    }
+
+    info.fill_weird();
+    info
   }
 
-  use std::os::unix::ffi::OsStrExt;
-  let raw = out.unwrap().stdout;
-  let git_dir = PathBuf::from(OsStr::from_bytes(&raw[..raw.len() - 1]));
+  fn fill_weird(&mut self) {
+    let out = Command::new("git")
+      .args(&["rev-parse", "--git-dir"])
+      .output();
 
-  for f in &[
-    "rebase-apply",
-    "rebase-merge",
-    "MERGE_HEAD",
-    "CHERRY_PICK_HEAD",
-    "REVERT_HEAD",
-  ] {
-    if git_dir.join(f).exists() {
-      return true;
+    maybe_bail(&out);
+
+    use std::ffi::OsStr;
+    use std::os::unix::ffi::OsStrExt;
+    use std::path::PathBuf;
+
+    let raw = out.unwrap().stdout;
+    let git_dir = PathBuf::from(OsStr::from_bytes(&raw[..raw.len() - 1]));
+
+    for f in &[
+      "rebase-apply",
+      "rebase-merge",
+      "MERGE_HEAD",
+      "CHERRY_PICK_HEAD",
+      "REVERT_HEAD",
+    ] {
+      if git_dir.join(f).exists() {
+        self.is_weird = true;
+        return;
+      }
     }
   }
-
-  false
 }
 
-fn bail() -> ! {
-  println!("0");
-  exit(0);
+impl std::fmt::Display for PromptInfo {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> Result<(), std::fmt::Error> {
+    let detached = self.head == "(detached)";
+
+    let prep = if detached { "at" } else { "on" };
+    let branch = if detached { &self.sha } else { &self.head };
+
+    return write!(
+      f,
+      "1 {} {} {} {}",
+      prep,
+      branch,
+      if self.is_dirty { 1 } else { 0 },
+      if self.is_weird { 1 } else { 0 },
+    );
+  }
+}
+
+fn maybe_bail(out: &io::Result<Output>) {
+  if out.is_err() || !out.as_ref().unwrap().status.success() {
+    println!("0");
+    exit(0);
+  }
 }
