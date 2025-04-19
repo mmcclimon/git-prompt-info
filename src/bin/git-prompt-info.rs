800dec7e8b650839f1687e4214b5c58eb67c55ca
Michael McClimon <michael@mcclimon.org>
2025-04-19 12:53:43 -0400

    prompt-info: output actual text with colors
    This is still half-done, but will get better shortly.

--- src/bin/git-prompt-info.rs
+++ src/bin/git-prompt-info.rs
@@ -1,5 +1,6 @@
 use std::io;
 use std::process::{exit, Command, Output};
+use termcolor::{BufferWriter, ColorChoice, WriteColor};
 
 #[derive(Debug, Default)]
 struct PromptInfo {
@@ -9,9 +10,17 @@ struct PromptInfo {
   is_weird: bool,
 }
 
-fn main() {
+fn main() -> Result<(), std::io::Error> {
   let info = PromptInfo::new();
   println!("{}", info);
+
+  let bufwtr = BufferWriter::stdout(ColorChoice::Always);
+  let mut buffer = bufwtr.buffer();
+
+  info.write_to(&mut buffer)?;
+  bufwtr.print(&buffer)?;
+
+  Ok(())
 }
 
 impl PromptInfo {
@@ -79,6 +88,32 @@ impl PromptInfo {
       }
     }
   }
+
+  pub fn write_to(&self, buffer: &mut termcolor::Buffer) -> std::io::Result<()> {
+    use std::io::Write;
+
+    let detached = self.head == "(detached)";
+
+    buffer.set_color(&colors::gray())?;
+    write!(buffer, "{} ", if detached { "at" } else { "on" })?;
+
+    buffer.set_color(&colors::teal())?;
+    write!(buffer, "{}", if detached { &self.sha } else { &self.head })?;
+
+    if self.is_weird {
+      buffer.set_color(&colors::warn())?;
+      write!(buffer, "!")?;
+    }
+
+    if self.is_dirty {
+      buffer.set_color(&colors::red())?;
+      write!(buffer, "*")?;
+    }
+
+    buffer.reset()?;
+
+    Ok(())
+  }
 }
 
 impl std::fmt::Display for PromptInfo {
@@ -103,5 +138,33 @@ fn maybe_bail(out: &io::Result<Output>) {
   if out.is_err() || !out.as_ref().unwrap().status.success() {
     println!("0");
     exit(0);
+  }
+}
+
+mod colors {
+  use termcolor::{Color, ColorSpec};
+
+  const GRAY: Color = Color::Rgb(0x92, 0x83, 0x74);
+  const TEAL: Color = Color::Rgb(0x42, 0x7b, 0x58);
+  const WARN: Color = Color::Rgb(0xd7, 0x99, 0x21);
+  const RED: Color = Color::Rgb(0xcc, 0x24, 0x1d);
+
+  fn color(which: Color) -> ColorSpec {
+    let mut c = ColorSpec::new();
+    c.set_fg(Some(which));
+    c
+  }
+
+  pub fn gray() -> ColorSpec {
+    color(GRAY)
+  }
+  pub fn teal() -> ColorSpec {
+    color(TEAL)
+  }
+  pub fn warn() -> ColorSpec {
+    color(WARN)
+  }
+  pub fn red() -> ColorSpec {
+    color(RED)
   }
 }
